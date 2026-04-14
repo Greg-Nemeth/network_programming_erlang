4b020fa9500379ca0d5895c1a2015b0e14a3338a
Greg Nemeth <gregmix88@gmail.com>
2026-04-14 23:07:37 +0100

    message encoding and related unit tests

--- tcp/chat/src/chat_protocol.erl
+++ tcp/chat/src/chat_protocol.erl
@@ -1,22 +1,25 @@
 -module(chat_protocol).
 
--export([decode_message/1]).
+-export([decode_message/1, encode_message/1]).
 -include("types.hrl").
 -type message() :: register() | broadcast().
 
--spec decode_message(binary()) -> {ok, message(), binary()}
+-spec decode_message(Data :: binary()) -> {ok, message(), binary()}
     | error
     | incomplete.
-decode_message(<<1, Rest/binary>>) ->
-    decode_register(Rest);
-decode_message(<<2, Rest/binary>>) ->
-    decode_broadcast(Rest);
-decode_message(<<>>) ->
-    incomplete;
-decode_message(<<_/binary>>) ->
-    error.
+decode_message(<<1, Rest/binary>>) -> decode_register(Rest);
+decode_message(<<2, Rest/binary>>) -> decode_broadcast(Rest);
+decode_message(<<>>) -> incomplete;
+decode_message(<<_/binary>>) -> error.
 
+-spec encode_message(Message :: message()) -> iodata().
+encode_message(#register{} = Msg) ->
+    [1, encode_string(Msg#register.username)];
+encode_message(#broadcast{} = Msg) ->
+    [2, encode_string(Msg#broadcast.from_username), encode_string(Msg#broadcast.contents)].
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% INTERNALS %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+ 
 decode_register(<<
         UsernameLen:16,
         Username:UsernameLen/binary,
@@ -33,6 +36,11 @@ decode_broadcast(<<
     {ok, #broadcast{from_username = Username, contents = Contents}, Rest};
 decode_broadcast(<<_/binary>>) -> incomplete.
 
+encode_string(Str) ->
+    [<<(byte_size(Str)):16>>, Str].
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TESTS %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 -ifdef('EUNIT').
 -include_lib("eunit/include/eunit.hrl").
 
@@ -43,7 +51,19 @@ decode_message_test_() ->
         {"CASE: returns incomplete for empty data", fun() -> ?assertEqual(incomplete, decode_message(~""))end},
         {"CASE: returns error for unknown message types", fun() -> ?assertEqual(error, decode_message(<<3, "rest">>))end}
     ]}.
-
+encode_message_test_() ->
+    {inparallel, [
+       {"CASE: can encode Register messages", fun() ->
+            Message = #register{username = ~"meg"},
+            IoData = encode_message(Message),
+            ?assertEqual(<<1,0,3, "meg">>, iolist_to_binary(IoData))
+        end} ,
+       {"CASE: can encode Broadcast messages", fun() ->
+            Message = #broadcast{from_username = ~"meg", contents = ~"hi"},
+            IoData = encode_message(Message),
+            ?assertEqual(<<2,0,3, "meg", 0, 2, "hi">>, iolist_to_binary(IoData))
+        end} 
+    ]}.
 can_decode_register_messages() ->
     Binary = <<1,0,3, "meg", "rest">>,
     {ok, Message, Rest} = decode_message(Binary),
